9a867eab173f2cf2a826b2ad49440cd55445883d
Lakshmi5524 <webtech@gmail.com>
2021-09-07 10:25:30 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+// function click()
+
+// function click() {
+//     setTimeout(function () {
+//         alert("User Not Available")
+//         var btn = document.getElementById('type')
+//         btn.innerHTML = "Add the Users"
+//         console.log(btn.innerHTML)
+//     }, 1000)
+// }
+
+function fun() {
+    setInterval(() => {
+        alert("Users Not Available");
+
+    }, 1000);
+}
+
+function download() {
+    alert("Download its Faild")
+}
+
+function invite() {
+    alert("Happly invited Team Members")
+}
+
